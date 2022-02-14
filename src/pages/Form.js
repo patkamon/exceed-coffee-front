c2192a3410943e6c2a699ec86c44c56cd3a25959
patkamon <patkamon.a@ku.th>
2022-02-14 23:49:49 +0700

    set basic form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,15 +1,38 @@
 import React from 'react'
+import { getObjForm } from '../utils/form'
 
 const Form = () => {
+
+
+  function handleSubmit(e) {
+      e.preventDefault()
+      const data = getObjForm(e.target)
+      console.log(data)
+
+    
+  }
+
+
+
+
+
   return (
     <div>Form
 
-            <label for="name">Enter your Name:</label>
-            <input className='customer-name' id='name' placeholder='name'/><br/>
 
-            <label for="phone">Enter your phone number:</label>
-            <input className='phone-number' id='phone' type='tel' placeholder='ex.088-777-3333'pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
 
+    
+
+      <form onSubmit={handleSubmit}>
+   
+     
+        <input className='customer-name' name='name' placeholder='name' ></input><br/>
+        <input className='phone-number' name='tel' type='tel' placeholder='ex.088-777-3333'pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/> 
+        <label className='label-num-seat' for='seat'>num.seat</label>
+
+        <input className='num-seat' name='num-seat'  type="number" min="1" max="8" required></input>
+        <button className='submit-btn' type='submit'>Submit</button>
+      </form>
 
 
 
--- src/utils/form.js
+++ src/utils/form.js
@@ -1,6 +1,8 @@
 export function getObjForm(target) {
+  
     const data = new FormData(target)
     const dataObj = {}
+   
     data.forEach((value, key) => {
       dataObj[key] = value
     })
