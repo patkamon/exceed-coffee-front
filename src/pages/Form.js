f857a85f60164d5525faeb6d5cc3f6ba756474f1
patkamon <patkamon.a@ku.th>
2022-02-16 18:02:20 +0700

    connect api with form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -10,16 +10,20 @@ const Form = () => {
 
   const { setQueueInfo } = useAuth()
 
+
+
+
+
   function handleSubmit(e) {
       e.preventDefault()
       const data = getObjForm(e.target)
       console.log(data)
 
       // wating for backend
-      // queueLogin(data)
-      // .then((data) => {
-      // setQueueInfo(data.queue)
-      // })
+      queueLogin(data)
+      .then((data) => {
+      setQueueInfo(data.tel)
+      })
 
   }
 
@@ -29,14 +33,14 @@ const Form = () => {
       <h1>Starbook Shop</h1><br />
       <div className='wall'></div>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} >
 
 
         <div className='grid-container'>
         <input className='customer-name' name='name' type='text' placeholder='Name' ></input><br/>
-        <input className='phone-number' name='tel' type='tel' placeholder='ex.088-777-3333'pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/><br/> 
+        <input className='phone-number' name='phone' type='tel' placeholder='ex.088-777-3333'pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/><br/> 
 
-        <input className='num-seat' name='num-seat'  type="number" min="1" max="8" placeholder='seat' required></input>
+        <input className='num-seat' name='willsit'  type="number" min="1" max="8" placeholder='seat' required></input>
         <button className='submit-btn' type='submit' hidden>Submit</button>
         </div>
       </form>
--- src/service/auth.js
+++ src/service/auth.js
@@ -14,16 +14,9 @@ export async function adminLogin(obj) {
 }
 
 export async function queueLogin(obj) {
-
-  // change format from json object to x-www-urlencoded
-  var data = [];
-  for (var key in obj) {
-         if (obj.hasOwnProperty(key)) {
-               data.push(encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]))                  
-               console.log(key + " -> " + obj[key]);
-     }
-    }
-  data= data.join("&");
-  const res = await axios.post("here", data)
+  console.log(obj)
+  obj.willsit = parseInt(obj.willsit)
+  console.log(obj)
+  const res = await axios.post("https://ecourse.cpe.ku.ac.th/exceed01/api/pat/1/insertqueue", obj)
   return res.data
 }
