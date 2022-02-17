b818c9ef753d5682aeb5db624da3a4754523d638
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2022-02-17 14:55:40 +0700

    adทรื

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,11 +30,31 @@ const Home = () => {
 
   return (
     
+    
     <div>
 
-<div class="topnav">
-  <a class="active" href="/home">STARBOOK</a>
-</div>
+    <div class="topnav">
+      <a class="active" href="/home">STARBOOK</a>
+      <div class='linkk'>
+      <a href="http://localhost:3000/dashboard">Admin</a>
+      </div>
+    </div>
+{/* 
+    <script src="https://w7.pngwing.com/pngs/701/653/png-transparent-computer-icons-system-administrator-administrator-icon-silhouette-desktop-wallpaper-administrator-icon.png"></script> */}
+    {/* <link rel="stylesheet" href="https://w7.pngwing.com/pngs/701/653/png-transparent-computer-icons-system-administrator-administrator-icon-silhouette-desktop-wallpaper-administrator-icon.png"></link> */}
+
+    {/* <link rel="icon" type="./img/admin.jpeg" href="http://localhost:3000/dashboard" /> */}
+
+    {/* <i class="fa-solid fa-screwdriver-wrench"></i> */}
+    {/* <a href="http://localhost:3000/dashboard"> 
+    <ion-icon name="hammer-outline"></ion-icon>
+    </a>
+    
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+     */}
+ 
 
       Starbook Shop<br/>
 
