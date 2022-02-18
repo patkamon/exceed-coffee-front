2ba8748942fa1d4a368af9ebaaf4c68c08cae9e4
patkamon <patkamon.a@ku.th>
2022-02-18 13:40:34 +0700

    dashboard option ui

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Collapsible from 'react-collapsible'
 import { useLocation } from 'react-router-dom'
 import '../pages/style/Nav.css'
 const Nav = () => {
@@ -13,9 +14,20 @@ const Nav = () => {
 
   return (
     <div>
-<div class="topnav">
+<div className="topnav">
           <a className="active" href="/home">STARBOOK</a>
           {locate === '/home' && <a className="linkk" href="/login">ADMIN</a>}
+          {locate === '/dashboard' && <div className='dropdown'>
+          <button className="dropbtn">Dropdown
+      <i className="fa fa-caret-down"></i>  </button>
+
+      <div className="dropdown-content">
+      <input type='submit' value='clear all' ></input>
+      <input type='submit' value='logout' ></input>
+    </div>
+      
+      </div>}
+
          </div>
 
 
